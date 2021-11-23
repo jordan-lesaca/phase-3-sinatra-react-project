ce6b790974cbdcc1833ea6f9bb013747f89fb29e
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-11-22 18:57:06 -0800

    added create table to create artist migration file

--- db/migrate/20211123015822_create_artists.rb
+++ db/migrate/20211123015822_create_artists.rb
@@ -1,4 +1,9 @@
 class CreateArtists < ActiveRecord::Migration[6.1]
   def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :style
+      t.integer :age
+    end
   end
 end
