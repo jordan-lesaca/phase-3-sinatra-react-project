6a8c2697a46c608c7104bf151e3091514437dd1d
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-11-22 19:04:28 -0800

    added create table to create paintings

--- db/migrate/20211123030255_create_paintings.rb
+++ db/migrate/20211123030255_create_paintings.rb
@@ -1,4 +1,9 @@
 class CreatePaintings < ActiveRecord::Migration[6.1]
   def change
+    create_table :paintings do |t|
+      t.string :title
+      t.string :paint_type
+      t.integer :year_created
+    end
   end
 end
